fae5bfc29b56fbcab4919ea399030f1bcf06d3ee
Noe Jacob <66005579+Noe-Jacob@users.noreply.github.com>
2021-10-23 11:15:20 +0530

    Update main.c with sensor change and extra maze

--- main.c
+++ main.c
@@ -1,8 +1,8 @@
 #include "stdio.h"
 #include "stdlib.h"
-#include "sdl.h"
+#include "SDL.h"
 #include "SDL2_gfxPrimitives.h"
-#include "time.h"
+#include <sys/time.h>
 
 #include "formulas.h"
 #include "wall.h"
@@ -25,15 +25,17 @@ int main(int argc, char *argv[]) {
 
     struct Robot robot;
     struct Wall_collection *head = NULL;
-    int front_left_sensor, front_right_sensor=0;
-    clock_t start_time, end_time;
-    int msec;
+    int front_left_sensor = 0, front_right_sensor= 0, right_sensor = 0, left_sensor = 0;
+    struct timeval start_time, end_time;
+    gettimeofday(&start_time, 0);
+    unsigned long msec;
 
     // SETUP MAZE
     // You can create your own maze here. line of code is adding a wall.
     // You describe position of top left corner of wall (x, y), then width and height going down/to right
     // Relative positions are used (OVERALL_WINDOW_WIDTH and OVERALL_WINDOW_HEIGHT)
     // But you can use absolute positions. 10 is used as the width, but you can change this.
+    
     insertAndSetFirstWall(&head, 1,  OVERALL_WINDOW_WIDTH/2, OVERALL_WINDOW_HEIGHT/2, 10, OVERALL_WINDOW_HEIGHT/2);
     insertAndSetFirstWall(&head, 2,  OVERALL_WINDOW_WIDTH/2-100, OVERALL_WINDOW_HEIGHT/2+100, 10, OVERALL_WINDOW_HEIGHT/2-100);
     insertAndSetFirstWall(&head, 3,  OVERALL_WINDOW_WIDTH/2-250, OVERALL_WINDOW_HEIGHT/2+100, 150, 10);
@@ -47,6 +49,69 @@ int main(int argc, char *argv[]) {
     insertAndSetFirstWall(&head, 11,  OVERALL_WINDOW_WIDTH/2+100, OVERALL_WINDOW_HEIGHT/2+200, OVERALL_WINDOW_WIDTH/2-100, 10);
     insertAndSetFirstWall(&head, 12,  OVERALL_WINDOW_WIDTH/2+200, OVERALL_WINDOW_HEIGHT/2+100, OVERALL_WINDOW_WIDTH/2-100, 10);
 
+    /*
+    insertAndSetFirstWall(&head, 1,  0 + 15, 0 + 10, 610, 10);
+    insertAndSetFirstWall(&head, 2,  0 + 15, 150 + 10, 310, 10);
+    insertAndSetFirstWall(&head, 3,  0 + 15, 250 + 10, 60, 10);
+    insertAndSetFirstWall(&head, 4,  0 + 15, 0 + 10, 10, 160);
+    insertAndSetFirstWall(&head, 5,  0 + 15, 200 + 10, 10, 260);
+    insertAndSetFirstWall(&head, 6,  50 + 15, 150 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 7,  50 + 15, 250 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 8,  100 + 15, 100 + 10, 10, 160);
+    insertAndSetFirstWall(&head, 9,  50 + 15, 50 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 10,  50 + 15, 50 + 10, 60, 10);
+    insertAndSetFirstWall(&head, 11,  100 + 15, 0 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 12,  0 + 15, 350 + 10, 110, 10);
+    insertAndSetFirstWall(&head, 13,  100 + 15, 350 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 14,  100 + 15, 400 + 10, 60, 10);
+    insertAndSetFirstWall(&head, 15,  150 + 15, 350 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 16,  50 + 15, 400 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 17,  150 + 15, 350 + 10, 210, 10);
+    insertAndSetFirstWall(&head, 18,  250 + 15, 350 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 19,  350 + 15, 350 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 20,  350 + 15, 400 + 10, 160, 10);
+    insertAndSetFirstWall(&head, 21,  500 + 15, 350 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 22,  200 + 15, 400 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 23,  300 + 15, 400 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 24,  100 + 15, 300 + 10, 210, 10);
+    insertAndSetFirstWall(&head, 25,  150 + 15, 250 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 26,  250 + 15, 250 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 27,  300 + 15, 300 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 28,  150 + 15, 50 + 10, 110, 10);
+    insertAndSetFirstWall(&head, 29,  150 + 15, 50 + 10, 10, 110);
+    insertAndSetFirstWall(&head, 30,  200 + 15, 100 + 10, 160, 10);
+    insertAndSetFirstWall(&head, 31,  300 + 15, 50 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 32,  350 + 15, 0 + 10, 10, 110);
+    insertAndSetFirstWall(&head, 33,  400 + 15, 50 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 34,  400 + 15, 50 + 10, 60, 10);
+    insertAndSetFirstWall(&head, 35,  450 + 15, 0 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 36,  150 + 15, 200 + 10, 210, 10);
+    insertAndSetFirstWall(&head, 37,  200 + 15, 200 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 38,  300 + 15, 200 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 39,  300 + 15, 250 + 10, 60, 10);
+    insertAndSetFirstWall(&head, 40,  350 + 15, 250 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 41,  350 + 15, 300 + 10, 60, 10);
+    insertAndSetFirstWall(&head, 42,  400 + 15, 200 + 10, 10, 110);
+    insertAndSetFirstWall(&head, 43,  400 + 15, 200 + 10, 60, 10);
+    insertAndSetFirstWall(&head, 44,  450 + 15, 200 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 45,  450 + 15, 250 + 10, 60, 10);
+    insertAndSetFirstWall(&head, 46,  350 + 15, 150 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 46,  350 + 15, 150 + 10, 210, 10);
+    insertAndSetFirstWall(&head, 47,  450 + 15, 100 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 48,  500 + 15, 50 + 10, 60, 10);
+    insertAndSetFirstWall(&head, 49,  500 + 15, 50 + 10, 10, 160);
+    insertAndSetFirstWall(&head, 50,  500 + 15, 200 + 10, 110, 10);
+    insertAndSetFirstWall(&head, 51,  550 + 15, 200 + 10, 10, 210);
+    insertAndSetFirstWall(&head, 52,  400 + 15, 350 + 10, 60, 10);
+    insertAndSetFirstWall(&head, 53,  450 + 15, 300 + 10, 10, 60);
+    insertAndSetFirstWall(&head, 54,  450 + 15, 300 + 10, 110, 10);
+    insertAndSetFirstWall(&head, 55,  550 + 15, 100 + 10, 60, 10);
+    insertAndSetFirstWall(&head, 56,  600 + 15, 0 + 10, 10, 210);
+    insertAndSetFirstWall(&head, 57,  600 + 15, 250 + 10, 10, 210);
+    insertAndSetFirstWall(&head, 58,  0 + 15, 450 + 10, 610, 10);
+    */
+    
+    
     setup_robot(&robot);
     updateAllWalls(head, renderer);
 
@@ -56,14 +121,17 @@ int main(int argc, char *argv[]) {
         SDL_RenderClear(renderer);
 
         //Move robot based on user input commands/auto commands
-        if (robot.auto_mode == 1)
-            robotAutoMotorMove(&robot, front_left_sensor, front_right_sensor);
-        robotMotorMove(&robot);
+        if (!robot.crashed)
+         {
+            if (robot.auto_mode == 1)
+                robotAutoMotorMove(&robot, front_left_sensor, front_right_sensor, left_sensor, right_sensor);
+            robotMotorMove(&robot);
+         }
 
         //Check if robot reaches endpoint. and check sensor values
         if (checkRobotReachedEnd(&robot, OVERALL_WINDOW_WIDTH, OVERALL_WINDOW_HEIGHT/2+100, 10, 100)){
-            end_time = clock();
-            msec = (end_time-start_time) * 1000 / CLOCKS_PER_SEC;
+            gettimeofday(&end_time, 0);
+            msec = ((end_time.tv_sec - start_time.tv_sec)*1000)+(end_time.tv_usec - start_time.tv_usec)/1000;
             robotSuccess(&robot, msec);
         }
         else if(checkRobotHitWalls(&robot, head))
@@ -72,15 +140,24 @@ int main(int argc, char *argv[]) {
         else {
             front_left_sensor = checkRobotSensorFrontLeftAllWalls(&robot, head);
             if (front_left_sensor>0)
-                printf("Getting close on the left. Score = %d\n", front_left_sensor);
+                printf("Getting close on the front left. Score = %d\n", front_left_sensor);
 
             front_right_sensor = checkRobotSensorFrontRightAllWalls(&robot, head);
             if (front_right_sensor>0)
-                printf("Getting close on the right. Score = %d\n", front_right_sensor);
+                printf("Getting close on the front right. Score = %d\n", front_right_sensor);
+            
+            right_sensor = checkRobotSensorRightAllWalls(&robot, head);
+            if (right_sensor>0)
+                printf("Getting close on the right. Score = %d\n", right_sensor);
+            
+            left_sensor = checkRobotSensorLeftAllWalls(&robot, head);
+            if (left_sensor>0)
+                printf("Getting close on the left. Score = %d\n", left_sensor);
         }
         robotUpdate(renderer, &robot);
         updateAllWalls(head, renderer);
-
+        
+        // Manual Control
         // Check for user input
         SDL_RenderPresent(renderer);
         while(SDL_PollEvent(&event)){
@@ -105,7 +182,7 @@ int main(int argc, char *argv[]) {
             }
             if(state[SDL_SCANCODE_RETURN]){
                 robot.auto_mode = 1;
-                start_time = clock();
+                gettimeofday(&start_time, 0);
             }
         }
 
