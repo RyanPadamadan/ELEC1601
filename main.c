0d6c0b331e267f7a57166247cdf5c03531a31636
Noe Jacob <66005579+Noe-Jacob@users.noreply.github.com>
2021-11-11 10:26:11 +0530

    Update colour

--- main.c
+++ main.c
@@ -52,7 +52,6 @@ int main(int argc, char *argv[]) {
     insertAndSetFirstWall(&head, 11,  OVERALL_WINDOW_WIDTH/2+100, OVERALL_WINDOW_HEIGHT/2+200, OVERALL_WINDOW_WIDTH/2-100, 10);
     insertAndSetFirstWall(&head, 12,  OVERALL_WINDOW_WIDTH/2+200, OVERALL_WINDOW_HEIGHT/2+100, OVERALL_WINDOW_WIDTH/2-100, 10);
     */
-    
 
  int i;
     insertAndSetFirstWall(&head, 12,  120, 450, 10, 30);
@@ -112,184 +111,12 @@ int main(int argc, char *argv[]) {
     insertAndSetFirstWall(&head, 2,  530, 80, 10, 20);
     insertAndSetFirstWall(&head, 2,  530, 80, 110, 10);
     
-    
-    /*
-    // Complex maze
-    insertAndSetFirstWall(&head, 1,  0 + 15, 0 + 10, 610, 10);
-    insertAndSetFirstWall(&head, 2,  0 + 15, 150 + 10, 310, 10);
-    insertAndSetFirstWall(&head, 3,  0 + 15, 250 + 10, 60, 10);
-    insertAndSetFirstWall(&head, 4,  0 + 15, 0 + 10, 10, 160);
-    insertAndSetFirstWall(&head, 5,  0 + 15, 200 + 10, 10, 260);
-    insertAndSetFirstWall(&head, 6,  50 + 15, 150 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 7,  50 + 15, 250 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 8,  100 + 15, 100 + 10, 10, 160);
-    insertAndSetFirstWall(&head, 9,  50 + 15, 50 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 10,  50 + 15, 50 + 10, 60, 10);
-    insertAndSetFirstWall(&head, 11,  100 + 15, 0 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 12,  0 + 15, 350 + 10, 110, 10);
-    insertAndSetFirstWall(&head, 13,  100 + 15, 350 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 14,  100 + 15, 400 + 10, 60, 10);
-    insertAndSetFirstWall(&head, 15,  150 + 15, 350 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 16,  50 + 15, 400 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 17,  150 + 15, 350 + 10, 210, 10);
-    insertAndSetFirstWall(&head, 18,  250 + 15, 350 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 19,  350 + 15, 350 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 20,  350 + 15, 400 + 10, 160, 10);
-    insertAndSetFirstWall(&head, 21,  500 + 15, 350 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 22,  200 + 15, 400 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 23,  300 + 15, 400 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 24,  100 + 15, 300 + 10, 210, 10);
-    insertAndSetFirstWall(&head, 25,  150 + 15, 250 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 26,  250 + 15, 250 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 27,  300 + 15, 300 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 28,  150 + 15, 50 + 10, 110, 10);
-    insertAndSetFirstWall(&head, 29,  150 + 15, 50 + 10, 10, 110);
-    insertAndSetFirstWall(&head, 30,  200 + 15, 100 + 10, 160, 10);
-    insertAndSetFirstWall(&head, 31,  300 + 15, 50 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 32,  350 + 15, 0 + 10, 10, 110);
-    insertAndSetFirstWall(&head, 33,  400 + 15, 50 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 34,  400 + 15, 50 + 10, 60, 10);
-    insertAndSetFirstWall(&head, 35,  450 + 15, 0 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 36,  150 + 15, 200 + 10, 210, 10);
-    insertAndSetFirstWall(&head, 37,  200 + 15, 200 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 38,  300 + 15, 200 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 39,  300 + 15, 250 + 10, 60, 10);
-    insertAndSetFirstWall(&head, 40,  350 + 15, 250 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 41,  350 + 15, 300 + 10, 60, 10);
-    insertAndSetFirstWall(&head, 42,  400 + 15, 200 + 10, 10, 110);
-    insertAndSetFirstWall(&head, 43,  400 + 15, 200 + 10, 60, 10);
-    insertAndSetFirstWall(&head, 44,  450 + 15, 200 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 45,  450 + 15, 250 + 10, 60, 10);
-    insertAndSetFirstWall(&head, 46,  350 + 15, 150 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 46,  350 + 15, 150 + 10, 210, 10);
-    insertAndSetFirstWall(&head, 47,  450 + 15, 100 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 48,  500 + 15, 50 + 10, 60, 10);
-    insertAndSetFirstWall(&head, 49,  500 + 15, 50 + 10, 10, 160);
-    insertAndSetFirstWall(&head, 50,  500 + 15, 200 + 10, 110, 10);
-    insertAndSetFirstWall(&head, 51,  550 + 15, 200 + 10, 10, 210);
-    insertAndSetFirstWall(&head, 52,  400 + 15, 350 + 10, 60, 10);
-    insertAndSetFirstWall(&head, 53,  450 + 15, 300 + 10, 10, 60);
-    insertAndSetFirstWall(&head, 54,  450 + 15, 300 + 10, 110, 10);
-    insertAndSetFirstWall(&head, 55,  550 + 15, 100 + 10, 60, 10);
-    insertAndSetFirstWall(&head, 56,  600 + 15, 0 + 10, 10, 210);
-    insertAndSetFirstWall(&head, 57,  600 + 15, 250 + 10, 10, 210);
-    insertAndSetFirstWall(&head, 58,  0 + 15, 450 + 10, 610, 10);
-    */
-    /*
-    // Complex Maze 2
-    insertAndSetFirstWall(&head, 1, 0, 0, 5, OVERALL_WINDOW_HEIGHT);
-        insertAndSetFirstWall(&head, 2, 0, 0, OVERALL_WINDOW_WIDTH, 5);
-        insertAndSetFirstWall(&head, 3, 60, OVERALL_WINDOW_HEIGHT - 5, OVERALL_WINDOW_WIDTH - 60, 5);
-        insertAndSetFirstWall(&head, 4, OVERALL_WINDOW_WIDTH - 5, 0, 5, OVERALL_WINDOW_HEIGHT - 60);
-        insertAndSetFirstWall(&head, 5, 60, 64, 5, OVERALL_WINDOW_HEIGHT - 62);
-        insertAndSetFirstWall(&head, 6, 64, 60, OVERALL_WINDOW_WIDTH - 128, 5);
-        int l = 0;
-        int b = 0;
-        while (l < 60) {
-            if (l <= 12) {
-                insertAndSetFirstWall(&head, 7, OVERALL_WINDOW_WIDTH - 64 - l * 3, 60 + l * 5, 5, 5);
-                b = OVERALL_WINDOW_WIDTH - 64 - l * 3;
-            }
-            if ((l >= 13 && l <= 25) || (l >= 39 && l <= 51)) {
-                insertAndSetFirstWall(&head, 7, b + (l - 12) * 3, 60 + l * 5, 5, 5);
-            }
-            if (l >= 26 && l <= 38) {
-                insertAndSetFirstWall(&head, 7, OVERALL_WINDOW_WIDTH - 64 - (l - 26) * 3, 60 + l * 5, 5, 5);
-                b = OVERALL_WINDOW_WIDTH - 64 - l * 3;
-            }
-            if (l >= 52) {
-                insertAndSetFirstWall(&head, 7, OVERALL_WINDOW_WIDTH - 64 - (l - 52) * 3, 60 + l * 5, 5, 5);
-                b = OVERALL_WINDOW_WIDTH - 64 - l * 3;
-            }
-            l++;
-        }
-        insertAndSetFirstWall(&head, 8, OVERALL_WINDOW_WIDTH - 128, OVERALL_WINDOW_HEIGHT/2 + 120, 45, 5);
-        insertAndSetFirstWall(&head, 9, 124, OVERALL_WINDOW_HEIGHT - 60, OVERALL_WINDOW_WIDTH - 128, 5);
-        insertAndSetFirstWall(&head, 10, OVERALL_WINDOW_WIDTH - 128, OVERALL_WINDOW_HEIGHT/2 - 120, 5, OVERALL_WINDOW_HEIGHT - 240);
-        insertAndSetFirstWall(&head, 11, 128, 120, OVERALL_WINDOW_WIDTH/2 + 64, 5);
-        insertAndSetFirstWall(&head, 12, 128, 180, OVERALL_WINDOW_WIDTH/2, 5);
-        insertAndSetFirstWall(&head, 13, 62, OVERALL_WINDOW_HEIGHT/2, 192, 5);
-        insertAndSetFirstWall(&head, 14, OVERALL_WINDOW_WIDTH/2, OVERALL_WINDOW_HEIGHT/2, 64, 5);
-        insertAndSetFirstWall(&head, 15, OVERALL_WINDOW_WIDTH/2, OVERALL_WINDOW_HEIGHT/2 + 60, 69, 5);
-        insertAndSetFirstWall(&head, 16, OVERALL_WINDOW_WIDTH/2, OVERALL_WINDOW_HEIGHT/2, 5, 60);
-        insertAndSetFirstWall(&head, 17, OVERALL_WINDOW_WIDTH/2 + 64, OVERALL_WINDOW_HEIGHT/2, 5, 60);
-        insertAndSetFirstWall(&head, 18, OVERALL_WINDOW_WIDTH/2 + 128, OVERALL_WINDOW_HEIGHT/2 - 60, 5, OVERALL_WINDOW_HEIGHT/2 - 60);
-        insertAndSetFirstWall(&head, 19, OVERALL_WINDOW_WIDTH/2 - 64, OVERALL_WINDOW_HEIGHT/2 + 120, OVERALL_WINDOW_WIDTH/2 - 128, 5);
-        insertAndSetFirstWall(&head, 20, OVERALL_WINDOW_WIDTH/2 - 64, OVERALL_WINDOW_HEIGHT/2 + 60, 5, 60);
-        insertAndSetFirstWall(&head, 21, 128, OVERALL_WINDOW_HEIGHT/2 + 60, 128, 5);
-        insertAndSetFirstWall(&head, 22, 128, OVERALL_WINDOW_HEIGHT/2 + 60, 5, 60);
-        insertAndSetFirstWall(&head, 23, 128, OVERALL_WINDOW_HEIGHT/2 + 120, 64, 5);
-        insertAndSetFirstWall(&head, 24, OVERALL_WINDOW_WIDTH/2 - 128, OVERALL_WINDOW_HEIGHT/2 + 120, 5, 60);
-        */
-    
-    
-    /*
-    // Circular maze
-    int j=0; // Key value
-    // Adding a circle with radius 50
-    for (int i=-50; i<51; i+=1){
-        // Leaving a gap
-        if (-20>i || i>20) {
-            insertAndSetFirstWall(&head, j,  OVERALL_WINDOW_WIDTH/2+i, OVERALL_WINDOW_HEIGHT/2+sqrt(2500-i*i), 5, 5);
-            j+=1;
-        }
-        insertAndSetFirstWall(&head, j,  OVERALL_WINDOW_WIDTH/2+i, OVERALL_WINDOW_HEIGHT/2-sqrt(2500-i*i), 5, 5);
-        j+=1;
-    }
-    // Adding a circle with radius 125
-    for (int i=-125; i<126; i+=1){
-        // Leaving a few gaps
-        if ((70 > i || i > 100) && (-100 > i || i > -75)) {
-            insertAndSetFirstWall(&head, j,  OVERALL_WINDOW_WIDTH/2+i, OVERALL_WINDOW_HEIGHT/2-sqrt(15625-i*i), 5, 5);
-            j+=1;
-        }
-        // Leaving more gaps
-        if ((-100 > i || i > -75) && (110 > i || i > 121)) {
-            insertAndSetFirstWall(&head, j,  OVERALL_WINDOW_WIDTH/2+i, OVERALL_WINDOW_HEIGHT/2+sqrt(15625-i*i), 5, 5);
-            j+=1;
-        }
-        // Creating a wall between the circles
-        if (18<i && i<70) {
-          insertAndSetFirstWall(&head, j,  OVERALL_WINDOW_WIDTH/2+19+i, OVERALL_WINDOW_HEIGHT/2+19+i, 5, 5);
-          j+=1;
-        }
-        // Creating a wall between the circles
-        if (20<i && i<85) {
-          insertAndSetFirstWall(&head, j,  OVERALL_WINDOW_WIDTH/2+20+i, OVERALL_WINDOW_HEIGHT/2-15-i/1.5, 5, 5);
-          j+=1;
-        }
-    }
-    // Adding a circle with radius 200
-    for (int i=-200; i<201; i+=1){
-        // Leaving a gap
-        if (198 > i) {
-            insertAndSetFirstWall(&head, j,  OVERALL_WINDOW_WIDTH/2+i, OVERALL_WINDOW_HEIGHT/2+sqrt(40000-i*i), 5, 5);
-            insertAndSetFirstWall(&head, j+1,  OVERALL_WINDOW_WIDTH/2+i, OVERALL_WINDOW_HEIGHT/2-sqrt(40000-i*i), 5, 5);
-            j+=2;
-        }
-        // Creating two walls between the circles
-        if (125<i && i<195) {
-          insertAndSetFirstWall(&head, j,  OVERALL_WINDOW_WIDTH/2+i, (OVERALL_WINDOW_HEIGHT/2-2+(i/5)), 5, 5);
-          j+=1;
-        }
-    }
-    // Cleaining up the circles and adding the end corridoor
-    insertAndSetFirstWall(&head, j,  OVERALL_WINDOW_WIDTH/2-200, OVERALL_WINDOW_HEIGHT/2-12.5, 5, 30);
-    insertAndSetFirstWall(&head, j+1,  OVERALL_WINDOW_WIDTH/2-125, OVERALL_WINDOW_HEIGHT/2-10, 5, 25);
-    insertAndSetFirstWall(&head, j+2,  OVERALL_WINDOW_WIDTH/2+125, OVERALL_WINDOW_HEIGHT/2-10, 5, 25);
-    insertAndSetFirstWall(&head, j+3,  OVERALL_WINDOW_WIDTH/2+200, OVERALL_WINDOW_HEIGHT/2+35, 150, 5);
-    insertAndSetFirstWall(&head, j+4,  OVERALL_WINDOW_WIDTH/2+200, OVERALL_WINDOW_HEIGHT/2-35, 150, 5);
-    // Creating more walls between the circles
-    insertAndSetFirstWall(&head, j+5,  OVERALL_WINDOW_WIDTH/2, OVERALL_WINDOW_HEIGHT/2-200, 5, 75);
-    insertAndSetFirstWall(&head, j+6,  OVERALL_WINDOW_WIDTH/2-200, OVERALL_WINDOW_HEIGHT/2, 75, 5);
-    */
-    
     setup_robot(&robot);
     updateAllWalls(head, renderer);
 
     SDL_Event event;
     while(!done){
-        SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
 
         //Move robot based on user input commands/auto commands
@@ -302,7 +129,7 @@ int main(int argc, char *argv[]) {
 
         //Check if robot reaches endpoint. and check sensor values
         //Complex
-        if (checkRobotReachedEnd(&robot, 640, 20, 10, 60)){ //Maze 5
+        if (checkRobotReachedEnd(&robot, 120, 480, 100, 10)){ //Maze 6
             gettimeofday(&end_time, 0);
             msec = ((end_time.tv_sec - start_time.tv_sec)*1000)+(end_time.tv_usec - start_time.tv_usec)/1000;
             robotSuccess(&robot, msec);
